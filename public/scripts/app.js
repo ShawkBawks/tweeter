2f58481250c0bfa82e2054bed5a613e4a3c85719
Logan Roberts <lroberts77@gmail.com>
2019-10-10 00:10:17 +0000

    Forgot to commit for a long time.  Implemented steps 1-6 of day 3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,48 +6,48 @@
 
 // Fake data taken from initial-tweets.json
 $(document).ready(function() {
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+  $('#tweet-form').on("submit", function(event) {
+    event.preventDefault();
+    let tweetBody = $(this).serialize();
+    if (!$('.tweet-text-area').val()) {
+    alert("There needs to be something to tweet!");
+    }if ($('.tweet-text-area').val().length > 140){
+      alert("There are too many characters!");
+    }
+    $.ajax({
+      method: "POST",
+      url: '/tweets',
+      data: tweetBody
+    })
+    .then(loadTweets);
+  });
+  
+  const loadTweets = () => { 
+    $.get('/tweets', function(res){
+      renderTweets(res);
+    });
   }
-]
-
 const renderTweets = function(tweets) {
-// const $tweets = tweets.map();;
-//   createTweetElement(tweets);
-//   $('#tweets-container').append($tweets);
+  $('#tweets-container').empty();
 for(let tweet of tweets){
   let output = createTweetElement(tweet);
-  $(`#tweets-container`).append(output);
+  $(`#tweets-container`).prepend(output);
 }
-}
+};
 
 // loops through tweets
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
-
+const escape =  function(str) {
+  let div = $("<div>").text(str);
+  
+  return div[0].innerHTML;
+}
 
 const createTweetElement = function(tweet) {
-// let $tweet = $('<article>').addClass('tweet');
-console.log("did we make it")
+  let date = new Date(tweet.created_at).toDateString();
+  // console.log("did we make it")
+  // let $tweet = $('<article>').addClass('tweet');
   const $tweets = (`
     <article class='tweets'>
       <header>
@@ -55,9 +55,9 @@ console.log("did we make it")
         ${tweet.user.name}
       <a class='tweet-handle'>${tweet.user.handle}</a>
       </header>
-      <span class='tweet-body'>${tweet.content.text}</span>
+      <span class='tweet-body'>${escape(tweet.content.text)}</span>
       <footer>
-        <h6 class="tweet-age">${tweet.created_at}</h6>
+        <h6 class="tweet-age">${date}</h6>
         <div class='tweet-icon'>
           <i class="fa fa-flag"></i>
           <i class="fa fa-heart"></i>
@@ -69,6 +69,6 @@ console.log("did we make it")
 
   return $tweets;
 };
-renderTweets(data);
+// renderTweets();
 
 });
